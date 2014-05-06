5b8e5d8d6f966ed0225e48ad4793d15a8fdda926
Panda <jeremy@jeremyutting.com>
2014-05-06 22:37:31 +1200

    Device list adapter
    Implements the BL DeviceList Adapter, and a good chunk of the scanning
    activity also.

--- gen/com/jeremyutting/localactions/R.java
+++ gen/com/jeremyutting/localactions/R.java
@@ -24,12 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int device_address=0x7f080003;
+        public static final int device_list=0x7f080001;
+        public static final int device_name=0x7f080002;
         public static final int homeScreen_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_scan=0x7f030001;
+        public static final int listitem_device=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -38,9 +42,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050004;
+        public static final int hello_world=0x7f050005;
         public static final int home_button=0x7f050002;
         public static final int title_activity_scan=0x7f050003;
+        public static final int unknown_device=0x7f050004;
     }
     public static final class style {
         /** 
--- src/com/jeremyutting/localactions/ScanActivity.java
+++ src/com/jeremyutting/localactions/ScanActivity.java
@@ -1,17 +1,41 @@
 package com.jeremyutting.localactions;
 
+import java.util.ArrayList;
+
+import com.estimote.sdk.BeaconManager;
+
 import android.os.Bundle;
-import android.app.ListActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
-public class ScanActivity extends ListActivity {
+public class ScanActivity extends Activity {
+	
+	private static final String TAG = ScanActivity.class.getSimpleName();
+	private BeaconManager beacon_manager;
+	private LeDeviceListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		com.estimote.sdk.utils.L.enableDebugLogging(true);
+		adapter = new LeDeviceListAdapter();
+		ListView list = (ListView) findViewById(R.id.device_list);
+		list.setAdapter(adapter);
+		
+		//TODO configure beacon manager
+		
 		setContentView(R.layout.activity_scan);
+		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
@@ -48,5 +72,75 @@ public class ScanActivity extends ListActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
+	
+	// Adapter for holding devices found through scanning.
+    private class LeDeviceListAdapter extends BaseAdapter {
+        private ArrayList<BluetoothDevice> mLeDevices;
+        private LayoutInflater mInflator;
+ 
+        public LeDeviceListAdapter() {
+            super();
+            mLeDevices = new ArrayList<BluetoothDevice>();
+            mInflator = ScanActivity.this.getLayoutInflater();
+        }
+ 
+        public void addDevice(BluetoothDevice device) {
+            if(!mLeDevices.contains(device)) {
+                mLeDevices.add(device);
+            }
+        }
+ 
+        public BluetoothDevice getDevice(int position) {
+            return mLeDevices.get(position);
+        }
+ 
+        public void clear() {
+            mLeDevices.clear();
+        }
+ 
+        @Override
+        public int getCount() {
+            return mLeDevices.size();
+        }
+ 
+        @Override
+        public Object getItem(int i) {
+            return mLeDevices.get(i);
+        }
+ 
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+ 
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder viewHolder;
+            // General ListView optimization code.
+            if (view == null) {
+                view = mInflator.inflate(R.layout.listitem_device, null);
+                viewHolder = new ViewHolder();
+                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+ 
+            BluetoothDevice device = mLeDevices.get(i);
+            final String deviceName = device.getName();
+            if (deviceName != null && deviceName.length() > 0)
+                viewHolder.deviceName.setText(deviceName);
+            else
+                viewHolder.deviceName.setText(R.string.unknown_device);
+            viewHolder.deviceAddress.setText(device.getAddress());
+ 
+            return view;
+        }
+    }
+    
+    static class ViewHolder {
+        TextView deviceName;
+        TextView deviceAddress;
+    }
 }
