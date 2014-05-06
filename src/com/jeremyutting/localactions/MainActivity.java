65155aaf73906644b7177ba18762f8014f8c1bb7
Panda <jeremy@jeremyutting.com>
2014-05-06 17:37:35 +1200

    BLE setup code
    Adds the setup for the BLE adapter, and prompts the user to turn
    Bluetooth on if it's not.

--- src/com/jeremyutting/localactions/MainActivity.java
+++ src/com/jeremyutting/localactions/MainActivity.java
@@ -2,13 +2,31 @@ package com.jeremyutting.localactions;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	private final static int REQUEST_ENABLE_BT = 1;
+	private BluetoothAdapter bt_adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Set up BLE, get adapter
+		final BluetoothManager bt_manager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		bt_adapter = bt_manager.getAdapter();
+		
+		// Is BLE available? Enabled?
+		if (bt_adapter == null || !bt_adapter.isEnabled()) {
+			Intent enable_bt_intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enable_bt_intent, REQUEST_ENABLE_BT);
+		}
+		
 		setContentView(R.layout.activity_main);
 	}
 
