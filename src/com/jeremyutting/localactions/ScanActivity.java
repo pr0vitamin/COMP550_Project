acfc0922e8fb2b434bda6a5289486f94c673d9e9
Panda <jeremy@jeremyutting.com>
2014-05-28 00:04:17 +1200

    Proximity update + light state and color updating
    changed the project to use proximity rather than trilateration due to
    inaccuracy. Added polling of the lights state to determine on/off. Added
    ability to change lights color based on proximity to specific beacon.

--- src/com/jeremyutting/localactions/ScanActivity.java
+++ src/com/jeremyutting/localactions/ScanActivity.java
@@ -1,7 +1,13 @@
 package com.jeremyutting.localactions;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -9,13 +15,9 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,10 +27,15 @@ import com.estimote.sdk.Region;
 import com.estimote.sdk.Utils;
 import com.estimote.sdk.utils.L;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -37,6 +44,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,6 +52,12 @@ import android.support.v4.app.NavUtils;
 
 public class ScanActivity extends Activity {
 	
+	private String bridgeIP = "192.168.99.161";
+	private String hueAPIKey = "comp550app";
+	private Button stateButton;
+	private int nearestBeacon = 0;
+	private boolean lightOn = true;
+	private int bulbNumber = 2;
 	private static final String TAG = ScanActivity.class.getSimpleName();
 	private final static int REQUEST_ENABLE_BT = 1;
 	
@@ -60,7 +74,9 @@ public class ScanActivity extends Activity {
 		// Link this activity to the activity_scan.xml layout
 		setContentView(R.layout.activity_scan);
 		
-		L.enableDebugLogging(false);
+		L.enableDebugLogging(true);
+		
+		stateButton = (Button) findViewById(R.id.on_off_button);
 		
 		// Create our custom ListView adapter, and link it with our view
 		adapter = new LeDeviceListAdapter();
@@ -69,7 +85,7 @@ public class ScanActivity extends Activity {
 		
 		// Create and initialize Estimote BeaconManager, and set the scan period to 200ms 
 		beacon_manager = new BeaconManager(this);
-		beacon_manager.setForegroundScanPeriod(200, 0);
+		beacon_manager.setForegroundScanPeriod(500, 0);
 		// Ranging is the act of scanning for beacons
 		beacon_manager.setRangingListener(new BeaconManager.RangingListener() {
 			
@@ -87,6 +103,22 @@ public class ScanActivity extends Activity {
 			}
 		});
 		
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			/*Timer timer = new Timer();
+			timer.scheduleAtFixedRate(new TimerTask() {
+
+				@Override
+				public void run() {
+					new FetchAPITask().execute();
+				}
+				
+			}, 0, 2000);*/
+		} else {
+			// No network connection - do something.
+		}
+		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
@@ -98,6 +130,19 @@ public class ScanActivity extends Activity {
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
+	}
+	
+	public void lightOnOff(View view) {
+		new SendStateAPITask().execute();
+	}
+	
+	public void changeLightColor(View view) {
+		if (nearestBeacon == 0) {
+			Log.d(TAG, "beacon not set, returning");
+			return;
+		} else {
+			new SendColorStateAPITask().execute();
+		}
 	}
 
 	@Override
@@ -193,7 +238,7 @@ public class ScanActivity extends Activity {
 		// adds the latest distance to the queue, remove the oldest if there's more than 5
 		public void addDistance(Double distance) {
 			distances.add(distance);
-			if (distances.size() > 5) {
+			if (distances.size() > 2) {
 				distances.remove();
 			}
 		}
@@ -243,23 +288,39 @@ public class ScanActivity extends Activity {
                 return lhs.getMajor() - rhs.getMajor();
               }
             });
+            nearestBeacon = 0;
             // Add the new distance to the distance tracker
             for (Beacon beacon : beacons) {
-            	switch (beacon.getMajor()) {
-            		case 32333:
-            			distances[0].addDistance(Utils.computeAccuracy(beacon));
-            			break;
-            		case 33771:
-            			distances[1].addDistance(Utils.computeAccuracy(beacon));
-            			break;
-            		case 50133:
-            			distances[2].addDistance(Utils.computeAccuracy(beacon));
-            			break;
+            	double distance;
+            	
+            	if (beacon.getMajor() == 32333) {
+            		distances[0].addDistance(Utils.computeAccuracy(beacon));
+        			distance = distances[0].getDistance();
+        			if (distance < 2.0 && distance < distances[1].getDistance() && distance < distances[2].getDistance()) {
+    					getActionBar().setSubtitle("In range of 32333");
+    					nearestBeacon = 32333;
+    				}
+            	}
+            	else if (beacon.getMajor() == 33771) {
+            		distances[1].addDistance(Utils.computeAccuracy(beacon));
+        			distance = distances[1].getDistance();
+        			if (distance < 2.0 && distance < distances[0].getDistance() && distance < distances[2].getDistance()) {
+    					getActionBar().setSubtitle("In range of 33771");
+    					nearestBeacon = 33771;
+    				}
+            	}
+            	else if (beacon.getMajor() == 50133) {
+            		distances[2].addDistance(Utils.computeAccuracy(beacon));
+        			distance = distances[2].getDistance();
+        			if (distance < 2.0 && distance < distances[0].getDistance() && distance < distances[1].getDistance()) {
+    					getActionBar().setSubtitle("In range of 50133");
+    					nearestBeacon = 50133;
+    				}
             	}
             }
-            
-            // This is where we want to do the trilateration.
-            GetCoordinates(distances[0].getDistance(), distances[1].getDistance(), distances[2].getDistance());
+            if (nearestBeacon == 0) {
+            	getActionBar().setSubtitle("No Beacons in range");
+            }
             
             notifyDataSetChanged();
         }
@@ -320,6 +381,7 @@ public class ScanActivity extends Activity {
          * Now with more actually working! 
          * Still need to take the distance as a ratio of the maximum, for best results.
          */
+        /*
         public void GetCoordinates(Double d1, Double d2, Double d3) {
         	// Constants to make array indices more explicit
         	final int X = 0;
@@ -328,8 +390,8 @@ public class ScanActivity extends Activity {
         	double[] origin = {0.0d, 0.0d};
         	// Declare our sources (the estimotes)
         	double[] first = origin;
-        	double[] second = {0.0d, 20.0d};
-        	double[] third = {20.0d, 0.0d};
+        	double[] second = {0.0d, 1.0d};
+        	double[] third = {1.0d, 0.0d};
         	// Declare our coordinates
         	double[] sensor = {0.0d, 0.0d};
         	
@@ -358,51 +420,55 @@ public class ScanActivity extends Activity {
         	
         	getActionBar().setSubtitle("x: " + String.format("%.1f", sensor[X]) + ", y: " + String.format("%.1f", sensor[Y]));
         	Log.d(TAG, "x: " + String.format("%.1f", sensor[X]) + ", y: " + String.format("%.1f", sensor[Y]));
-        }
+        } */
         
         /*
          * Helper function to do basic matrix subtractions
          */
+        /*
         public double[] matrixSub(double[] a, double[] b) {
         	double[] result = {0.0d, 0.0d};
         	for (int i=0; i<2; i++) {
         		result[i] = a[i] - b[i];
         	}
         	return result;
-        }
+        } */
         
         /*
          *  Helper function to do basic matrix divisions by a scalar
          */
+        /*
         public double[] matrixDiv(double[] a, double scalar) {
         	double[] result = {0.0d, 0.0d};
         	for (int i=0; i<2; i++) {
         		result[i] = a[i]/scalar;
         	}
         	return result;
-        }
+        } */
         
         /*
          * Helper function to do basic matrix multiplications by a scalar
          */
+        /*
         public double[] matrixMult(double[] a, double scalar) {
         	double[] result = {0.0d, 0.0d};
         	for (int i=0; i<2; i++) {
         		result[i] = a[i]*scalar;
         	}
         	return result;
-        }
+        } */
         
         /*
          * Helper function to do basic matrix dot products
          */
+        /*
         public double matrixDot(double[] a, double[] b) {
         	double result = 0.0d;
         	for (int i=0; i<2; i++) {
         		result += a[i] * b[i];
         	}
         	return result;
-        }
+        } */
     }
     
     static class ViewHolder {
@@ -410,79 +476,152 @@ public class ScanActivity extends Activity {
         TextView deviceDistance;
     }
     
-    public static LightState parseState(JSONObject jsonObj) {
+    private class SendStateAPITask extends AsyncTask<Void, Void, String> {
 
-		LightState state = new LightState();
-
-		try{
-			JSONArray state_list = jsonObj.getJSONArray("state");
-			JSONObject stat = state_list.getJSONObject(0);
-			state.hue = stat.optInt("hue");
-			state.on = stat.optBoolean("on");
-			state.brightness = stat.optInt("bri");
-			state.saturation = stat.optInt("sat");
-
-		} catch(JSONException e) {
-			Log.d(TAG, "JSON exception when parsing light state");
+		@Override
+		protected String doInBackground(Void... urls) {
+			String requestData;
+			if (lightOn) {
+				requestData = "{\"on\": false}";
+			} else {
+				requestData = "{\"on\": true}";
+			}
+			String urlString = "http://" + bridgeIP + "/api/" + hueAPIKey + "/lights/" + bulbNumber + "/state";
+			try {
+				URL url = new URL(urlString);
+				HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+				httpCon.setDoOutput(true);
+				httpCon.setRequestMethod("PUT");
+				OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+				out.write(requestData);
+				out.close();
+				httpCon.getInputStream();
+			} catch (MalformedURLException e) {
+				Log.e(TAG, "Malformed URL: " + e);
+			} catch (IOException e) {
+				Log.e(TAG, "SendStateAPITask IOException: " + e);
+			}
+			return null;
 		}
-		return state;
-	}
+    }
     
-    public static void parseSetStateResponse(URI url, int bri,int hue,int sat, boolean on) {
-    	try {
-    		// 1. create HttpClient
-			HttpClient httpclient = new DefaultHttpClient();
+    private class SendColorStateAPITask extends AsyncTask<Void, Void, String> {
 
-			// 2. make POST request to the given URL
-			HttpPost httpPost = new HttpPost(url);
+		@Override
+		protected String doInBackground(Void... urls) {
+			String requestData;
+			Log.d(TAG, "nearestBeacon: " + nearestBeacon);
+			if (nearestBeacon == 32333) {
+				//Blue
+				requestData = "{\"hue\": 45745, \"bri\": 254, \"sat\": 254}";
+			} else if (nearestBeacon == 33771) {
+				//Red
+				requestData = "{\"hue\": 65000, \"bri\": 254, \"sat\": 254}";
+			} else if (nearestBeacon == 50133) {
+				//Pink
+				requestData = "{\"hue\": 55212, \"bri\": 254, \"sat\": 254}";
+			} else {
+				return null;
+			}
+			String urlString = "http://" + bridgeIP + "/api/" + hueAPIKey + "/lights/" + bulbNumber + "/state";
+			try {
+				URL url = new URL(urlString);
+				HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+				httpCon.setDoOutput(true);
+				httpCon.setRequestMethod("PUT");
+				OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+				out.write(requestData);
+				out.close();
+				httpCon.getInputStream();
+			} catch (MalformedURLException e) {
+				Log.e(TAG, "Malformed URL: " + e);
+			} catch (IOException e) {
+				Log.e(TAG, "SendColorStateAPITask IOException: " + e);
+			}
+			return null;
+		}
+    }
+    
+    private class FetchAPITask extends AsyncTask<Void, Void, String> {
 
-			String json = "";
-
-
-			JSONObject jsonObject = new JSONObject();
-
-			jsonObject.accumulate("hue", hue);
-			jsonObject.accumulate("on", on);
-			jsonObject.accumulate("bri", bri);
-			jsonObject.accumulate("sat", sat);
-
-
-			json = jsonObject.toString();
-			Log.d(TAG, "json: " + json);
-
-			// 5. set json to StringEntity
-			StringEntity se = new StringEntity(json);
-
-			// 6. set httpPost Entity
-			httpPost.setEntity(se);
-
-			// 7. Set some headers to inform server about the type of the content   
-			httpPost.setHeader("Accept", "application/json");
-			httpPost.setHeader("Content-type", "application/json");
-
-			// 8. Execute POST request to the given URL
-			HttpResponse httpResponse = httpclient.execute(httpPost);
+		@Override
+		protected String doInBackground(Void... urls) {
+			try {
+				return fetchStateFromAPI(bulbNumber);
+			} catch (IOException e) {
+				Log.e(TAG, "FetchAPITask IOException: " + e);
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			//Log.d(TAG, "The string is: " + result);
+			if (result == null) {
+				return;
+			}
+			JSONObject jObject = null;
+			try {
+				jObject = new JSONObject(result);
+			} catch (JSONException e) {
+				Log.e(TAG, "Error converting result: " + e);
+			}
+			//Log.d(TAG, "The json object is: " + jObject.toString());
 			
-		} catch (Exception e) {
-			Log.d(TAG, "Stack trace: " + e);
-    	}
-	}
-    
-    public void lightOnOff(View view) {
-    	URI uri = null;
-		try {
-			uri = new URI("http://192.168.99.161/");
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JSONObject lightState = null;
+			try {
+				lightState = jObject.getJSONObject("state");
+				try {
+					String state = lightState.getString("on");
+					//Log.d(TAG, "Light state: " + state);
+					if (state.equals("true")) {
+						lightOn = true;
+						stateButton.setText("Off");
+					} else {
+						lightOn = false;
+						stateButton.setText("On");
+					}
+				} catch (JSONException e) {
+					Log.e(TAG, "Error converting result: " + e);
+				}
+			} catch (JSONException e) {
+				Log.e(TAG, "Error converting result: " + e);
+			}
 		}
-    	parseSetStateResponse(uri, 1, 2, 3, true);
-    }
-    
-    static class LightState {
-    	public int hue;
-    	public boolean on;
-    	public int brightness;
-    	public int saturation;
-    }
+	}
+	
+	private String fetchStateFromAPI(int bulb) throws IOException {
+		String myUrl = "http://" + bridgeIP + "/api/" + hueAPIKey + "/lights/" + bulb;
+		//Log.d(TAG, "URL: " + myUrl);
+		InputStream is = null;
+		try {
+			URL url = new URL(myUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setReadTimeout(2000);
+			conn.setConnectTimeout(10);
+			conn.setRequestMethod("GET");
+			conn.setDoInput(true);
+			conn.connect();
+			int response = conn.getResponseCode();
+			//Log.d(TAG, "The response is: " + response);
+			is = conn.getInputStream();
+			
+			String content = readIt(is);
+			return content;
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
+	
+	public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int length = 0;
+		while ((length = stream.read(buffer)) != -1) {
+			baos.write(buffer, 0, length);
+		}
+		return new String(baos.toByteArray(), "UTF-8");
+	}
 }
